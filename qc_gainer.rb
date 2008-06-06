b94cb08c70b05fb45bf1828ec951505c1f2536ed
mootoh <mootoh@gmail.com>
2008-06-06 17:37:33 +0900

    it works fine :)

--- qc_gainer.rb
+++ qc_gainer.rb
@@ -2,7 +2,6 @@
 #  Created by mootoh on 6/3/08.
 #  Copyright (c) 2008 deadbeaf.org. All rights reserved.
 #
-require 'logger'
 require 'osx/cocoa'
 OSX.require_framework 'QuartzComposer'
 require 'funnel'
@@ -13,49 +12,47 @@ class QCGainer < OSX::QCPlugIn
   end
 
   def self.timeMode
-    0
+    1
   end
 
   def initialize
-    @logger = Logger.new('/tmp/qc_ruby_plugin.log')
-    @logger.level = Logger::DEBUG
-    @yet = true
+    @initialized = false
     @gio = Funnel::Gainer.new(Funnel::Gainer::MODE1)
+
+    @ain = [0,0,0,0]
+    @din = [0,0,0,0]
+    @aot = [0,0,0,0]
+    @dot = [0,0,0,0]
   end
 
   def startExecution(context)
-    @logger.debug("startExecution")
-
-    Thread.new {
+    Thread.new do
       sleep 0.1
-      5.times do |i|
-        begin
-          addInputPortWithType_forKey_withAttributes(OSX::QCPortTypeString, "input_" + i.to_s, nil)
-          addOutputPortWithType_forKey_withAttributes(OSX::QCPortTypeString, "out_" + i.to_s, nil)
-          @logger.debug("ports added")
-        rescue => e
-          @logger.error(e.message)
-        end
-      end
-    }
+      4.times { |i| addInputPortWithType_forKey_withAttributes(OSX::QCPortTypeNumber, "aot_" + i.to_s, nil)  }
+      4.times { |i| addInputPortWithType_forKey_withAttributes(OSX::QCPortTypeNumber, "dot_" + i.to_s, nil)  }
+      4.times { |i| addOutputPortWithType_forKey_withAttributes(OSX::QCPortTypeNumber, "ain_" + i.to_s, nil) }
+      4.times { |i| addOutputPortWithType_forKey_withAttributes(OSX::QCPortTypeNumber, "din_" + i.to_s, nil) }
+    end
     true
   end
 
   def execute_atTime_withArguments(context, time, args)
-    @logger.debug("execute !")
-
-    if @yet
+    unless @initialized
       @gio.ain(0).on Funnel::PortEvent::CHANGE do |event|
-        setValue_forOutputKey(event.target.value, "out_0")
+        @ain[0] = event.target.value
       end
-      @yet = false
+      @initialized = false
+    end
+
+    4.times do |i|
+      self.setValue_forOutputKey(@ain[i], "ain_" + i.to_s)
+      self.setValue_forOutputKey(@din[i], "din_" + i.to_s)
     end
 
     true
   end
 
   def stopExecution(context)
-    @logger.debug("stopExecution")
     true
   end
 end
